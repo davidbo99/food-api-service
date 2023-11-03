6a61c4f3fe201e903d4df554761fd5e7d97a3897
luis sanabria <luisalbertosanabriamendez@gmail.com>
2023-11-02 22:08:17 -0500

    Correction the logic problem in the endpoint

--- recetasback/main.py
+++ recetasback/main.py
@@ -1,23 +1,22 @@
 from fastapi import FastAPI, HTTPException
 import requests
+import uvicorn
 
 app = FastAPI()
 
 
 @app.get("/recipes/")
-def get_recipes(nutrients: str):
-    """
-    Obtén recetas basadas en un nutriente específico.
-    """
-    url = f"https://api.spoonacular.com/recipes/\
-    findByNutrients?nutrients={nutrients}"
-    response = requests.get(url)
+def get_recipes(minCalories: int, maxCalories: int):
+    # Obtén recetas basadas en un rango de calorías específico.
+    url = f"https://api.spoonacular.com/recipes/findByNutrients?\
+        minCalories={minCalories}&maxCalories={maxCalories}"
+    headers = {'x-api-key': 'b31d2b784b94458f80d709dfb07840ed'}
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
         return response.json()
     else:
-        raise HTTPException(status_code=404, detail="Company doesn'texists")
+        raise HTTPException(status_code=404, detail="Error")
 
 
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
