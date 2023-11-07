0e23c31e0830f5e0a55e1c9b63ccec1f46676f52
luis sanabria <luisalbertosanabriamendez@gmail.com>
2023-11-07 11:20:57 -0500

    update for connection with Angular

--- recetasback/main.py
+++ recetasback/main.py
@@ -1,13 +1,20 @@
 # importaciones base para el endpoint
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import random_recipe_routers
-
 import uvicorn
 
 
 app = FastAPI()
 
+# Configuración del middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(random_recipe_routers.router)
 
