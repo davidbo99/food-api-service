8bed531ee9dcd40f610d315ed4457a556cca4c46
José Gregorio Calderón Buitrago <jose_calderon@THOT>
2023-11-07 21:46:02 -0500

    Se remueven comentarios verbosos

--- recetasback/dependencies/database.py
+++ recetasback/dependencies/database.py
@@ -1,6 +1,6 @@
 """Módulo de bases de datos"""
 
-# Importaciones necesarias para manejo de contextos, enumeraciones y SQL Alchemy para ORM.
+
 from contextlib import contextmanager
 from enum import Enum, auto
 from sqlalchemy import create_engine
@@ -9,49 +9,39 @@ from sqlalchemy.orm import sessionmaker
 
 import logging
 
-# Configuración del sistema de registro de eventos (logging) a nivel de información.
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Define una enumeración para representar los posibles estados de la base de datos.
+
 class HealthStatus(Enum):
-    CONNECTED = auto()      # Estado cuando la base de datos está conectada.
-    DISCONNECTED = auto()   # Estado cuando la base de datos está desconectada.
-    UNKNOWN = auto()        # Estado cuando el estado de la base de datos es desconocido.
+    CONNECTED = auto()     
+    DISCONNECTED = auto()  
+    UNKNOWN = auto()
 
-# Define una clase para manejar la conexión con la base de datos.
+
 class Database:
-
+    # Define una clase para manejar la conexión con la base de datos y métodos para obtener un status de esta"""
+    
     def __init__(self, db_url: str) -> None:
-        # Al inicializar la clase, crea un motor de base de datos con la URL proporcionada y lo configura para mostrar las consultas en consola.
         self._engine = create_engine(db_url, echo=True)
-        # Crea una fábrica de sesiones de base de datos vinculada al motor creado.
         self._db = sessionmaker(autocommit=False, autoflush=False, bind=self._engine)
         
     def get_status(self) -> HealthStatus:
-        # Método para obtener el estado actual de la base de datos.
         try:
-            # Intenta crear una conexión temporal con la base de datos.
             with self._engine.connect() as conn:
-                # Si la conexión es exitosa, devuelve el estado de CONECTADO.
                 return HealthStatus.CONNECTED
         except SQLAlchemyError as e:
-            # Si hay un error de SQLAlchemy, registra el error y devuelve el estado de DESCONECTADO.
             logger.error(f"Error al conectar a la base de datos: {e}")
             return HealthStatus.DISCONNECTED
         except Exception as e:
-            # Para cualquier otro error inesperado, registra el error y devuelve el estado de DESCONOCIDO.
             logger.error(f"Estado desconocido de la base de datos: {e}")
             return HealthStatus.UNKNOWN
 
     @contextmanager
-    # Contextmanager para gestionar la sesión de la base de datos.
     def get_db(self):
-        # Inicializa una sesión de base de datos.
         db = self._db()
         try:
-            # Cede control al bloque de código que invoca este método con el objeto de sesión.
             yield db
         finally:
-            # Asegura que la sesión se cierre después de su uso para evitar conexiones abiertas.
             db.close()
--- recetasback/main.py
+++ recetasback/main.py
@@ -4,26 +4,14 @@ from fastapi import FastAPI
 from recetasback.dependencies.containers import Container
 from recetasback.routers import endpoints
 
-# Crea una instancia de la aplicación FastAPI.
+
 app = FastAPI()
 
-# Este bloque solo se ejecuta si el script se ejecuta directamente (es decir, no se importa como un módulo).
-if __name__ == '__main__':
-    
-    # Crea una instancia de la clase Container del módulo recetasback.dependencies.containers.
-    container = Container()
-    
-    """Utiliza la inyección de dependencias para conectar cualquier dependencia necesaria para los módulos especificados.
-    En este caso, está conectando dependencias para el módulo actual (__name__ se refiere al nombre del módulo actual)"""
-    container.wire(modules=[__name__])
 
-    """Recupera la dependencia de base de datos del contenedor. Se espera que la propiedad 'db' proporcione
-    acceso a la sesión/conexión de la base de datos."""
+if __name__ == '__main__':
+    """Punto de inicio en el que se crea el contenedor, la inyección de la base de datos y el sistema de rutas"""
+    container = Container()
+    container.wire(modules=[__name__])
     db = container.db()
-    
-    # Asigna el contenedor al estado de la aplicación para que sea accesible en toda la aplicación.
     app.container = container
-    
-    """Incluye el enrutador del módulo 'endpoints' del paquete recetasback.adapters.
-    Esto añade todas las rutas de endpoints definidas en ese enrutador a la aplicación FastAPI."""
     app.include_router(endpoints.router)
