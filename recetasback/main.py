18d45cfd17f13d51b73c2c3df2c4c9ef2f8ae017
Stiven4729 <gallegostiven4729@gmail.com>
2023-11-09 12:13:58 -0500

    Cambios de endpoint

--- recetasback/main.py
+++ recetasback/main.py
@@ -9,6 +9,3 @@ import uvicorn
 app = FastAPI()
 
 app.include_router(recipe_routers.router)
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
--- recetasback/routers/recipe_routers.py
+++ recetasback/routers/recipe_routers.py
@@ -1,14 +1,16 @@
 from fastapi import APIRouter, HTTPException
+from starlette.responses import JSONResponse
 from ..services.recipe_service import get_recipes_by_nutrients
 
 router = APIRouter(prefix='/recipes')
 
-
 @router.get("/recipes")
 def get_recipes(min_calories: int = 0, max_calories: int = 20000):
     response = get_recipes_by_nutrients(min_calories, max_calories)
-    # validaciones para las respuestas de la petición
+    
+    # Validaciones para las respuestas de la petición
     if response.status_code == 200:
         return response.json()
     else:
-        raise HTTPException(status_code=404, detail="Error")
+        # Usar JSONResponse para personalizar el código de estado de la respuesta
+        return JSONResponse(content={"detail": "Error"}, status_code=response.status_code)
