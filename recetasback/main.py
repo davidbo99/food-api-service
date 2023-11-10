f90baafff7454180bc7c2e2b7dd3cafb68959c06
luis sanabria <luisalbertosanabriamendez@gmail.com>
2023-11-10 10:13:08 -0500

    updates for environments

--- recetasback/main.py
+++ recetasback/main.py
@@ -2,22 +2,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .routers import random_recipe_routers
-import uvicorn
-
+import os
 
 app = FastAPI()
 
-# Configuración del middleware CORS
+CONECTION = os.getenv("URL_CONECTION_FRONT")
+
+# Configuración del middleware CORS ()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
+    allow_origins=["CONECTION"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(random_recipe_routers.router)
-
-# ejecucion del codigo
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
